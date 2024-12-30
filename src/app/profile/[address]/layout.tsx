f69d4ab13ffe0059037f432c3a56adddf0c7bf94
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2024-12-30 20:46:19 -0300

    add opengraph image

---
+++ src/app/profile/[address]/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next'
+
+export async function generateMetadata({ params }: { params: { address: string } }): Promise<Metadata> {
+  return {
+    title: `Profile - ${params.address}`,
+    description: `View profile and stats for ${params.address}`,
+    openGraph: {
+      images: [`/profile/${params.address}/opengraph-image`],
+    },
+  }
+}
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+} 
