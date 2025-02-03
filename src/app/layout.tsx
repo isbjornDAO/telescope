36fda8d147a30a3773eab67418937854b1cd8b8c
Mach1Btc <allxcaps@gmail.com>
2025-02-03 14:22:01 -0600

    remove page, making it a tabs on main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,16 +10,16 @@ import { Web3Provider } from "@/components/providers/web3";
 import { Toaster } from "@/components/ui/toaster";
 import { Navbar } from "@/components/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url.base),
@@ -70,7 +70,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" enableSystem>
           <Web3Provider>
--- src/app/mint/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-
-export default function MintPage() {
-    return <h1>Mint Page</h1>
-}
