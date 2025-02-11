f75885301affd45e186d2316d30c9924cf94538b
Mach1Btc <allxcaps@gmail.com>
2025-02-11 14:55:28 -0600

    can now painc mint, on testnet

--- src/components/mint-window.tsx
+++ src/components/mint-window.tsx
@@ -1,25 +1,32 @@
 "use client";
 
-import { useAccount } from "wagmi";
+import { useAccount, usePrepareTransactionRequest, useReadContract, useSendTransaction } from "wagmi";
 import { ConnectButton } from "@/components/connect-button";
-import { MintButton } from "@/components/mint-button";
 import { WLRoundIndicator } from "@/components/wl-round-indicator";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MintCounter } from "@/components/mint-counter";
 import { useUserStats } from "@/hooks/use-user-stats";
-import { Address } from "viem";
+import { Address, encodeFunctionData, parseEther } from "viem";
 import { ConnectDiscordButton } from "./connect-discord-button";
 import { CheckCircle } from "lucide-react";
+import { Button } from "./ui/button";
+import { puppets_nft_abi, puppets_nft_address } from "@/lib/constants";
 
 
 export function MintWindow() {
     const { address, isConnected } = useAccount();
 
+    const MAX_SUPPLY = 1000;
+    const [numMinted, setNumMinted] = useState(250);
 
-    const [isMinting, setIsMinting] = useState(false);
+    const [isMinting, setIsMinting] = useState(true);
+    const [mintPhase, setMintPhase] = useState<number>(0);
     const [numToMint, setNumToMint] = useState<number>(1);
     const [maxAllowedToMint, setMaxAllowedToMint] = useState<number>(10);
-    const [roundJoined, setRoundJoined] = useState(5);
+    const [totalCost, setTotalCost] = useState(0);
+
+    const [whiteListLevel, setWhiteListLevel] = useState(6);
+    const [whiteListString, setWhiteListString] = useState<string>('Connect your wallet to see if you got WL');
 
     const { data: userStats, isLoading: isUserStatsLoading } = useUserStats(
         address as Address,
@@ -29,27 +36,104 @@ export function MintWindow() {
     const puppetPrices: { [round: number]: number } = {
         1: 1,
         2: 1.2,
-        3: 1.6,
-        4: 1.8,
-        5: 2
-    }
+        3: 1.4,
+        4: 1.6,
+        5: 1.8,
+        6: 2
+    };
+
+    const { data: mintPhaseData, refetch: fetchMintPhase } = useReadContract({
+        abi: puppets_nft_abi,
+        address: puppets_nft_address,
+        functionName: "getCurrentPhase",
+        chainId: 43113
+    });
+
+    const transactionData = encodeFunctionData({
+        abi: puppets_nft_abi,
+        functionName: whiteListLevel === 6 && mintPhase < 6 ? 'panicMint' :
+            mintPhase === 6 ? 'publicMint' : 'wlMint',
+        args: [BigInt(numToMint)]
+    });
+
+    const { data: request } = usePrepareTransactionRequest({
+        chainId: 43113,
+        account: address,
+        to: puppets_nft_address,
+        data: transactionData,
+        value: parseEther(totalCost.toString())
+    });
+
+    const { sendTransaction: sendMintTx, isPending: isMintPending } = useSendTransaction();
+
+    const handleMint = () => {
+        if (request) {
+            sendMintTx(request);
+        } else {
+            console.error("Failed to queue up mint tx.");
+        }
+    };
+
+    useEffect(() => {
+        if (mintPhase != 6) {
+            setMaxAllowedToMint(mintPhase);
+        } else {
+            setMaxAllowedToMint(10);
+        }
+    }, [mintPhase]);
+
+    useEffect(() => {
+        if (whiteListLevel < 6) {
+            setWhiteListString(`You are in round ${whiteListLevel}. You can also mint in each round after`);
+        } else {
+            setWhiteListString(`You are not on the list. Feel free to mint at the public price`);
+        }
+    }, [whiteListLevel]);
+
+    useEffect(() => {
+        if (whiteListLevel <= mintPhase) {
+            setTotalCost(puppetPrices[mintPhase] * numToMint);
+        } else {
+            setTotalCost(puppetPrices[6] * numToMint);
+        }
+    }, [numToMint, whiteListLevel, mintPhase]);
+
+    useEffect(() => {
+        if (mintPhaseData !== undefined) {
+            setMintPhase(Number(mintPhaseData));
+        }
+    }, [mintPhaseData]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchMintPhase();
+        }, 15000); // Poll every 15 seconds
+
+        return () => clearInterval(interval);
+    }, [fetchMintPhase]);
 
     return isMinting
         ? (
             <div className="w-full flex flex-col md:flex-row gap-1 pl-12 pr-8 items-center">
-                <img
-                    className="w-[300px] border-zinc-300 border-2"
-                    src="example_puppet.png"
-                />
+                <div className="flex flex-col gap-2 items-center">
+                    <img
+                        className="w-[300px] border-zinc-300 border-2"
+                        src="example_puppet.png"
+                    />
+                    <span className="font-semibold text-zinc-500">{`${numMinted} / ${MAX_SUPPLY} minted`}</span>
+                </div>
                 <div className="mt-2 md:mt-0 flex-1 text-center flex flex-col gap-1 items-center">
                     <h2 className="text-2xl font-bold mb-2">Mint a puppet</h2>
-                    <div className="w-[300px]">Welcome new student to Bear University! Those who picked up the avax pamphlet first have a lower enrollment fee!</div>
-                    <WLRoundIndicator roundJoined={roundJoined} prices={puppetPrices} />
+                    <div className="w-[300px] mb-2">{`Welcome new student to Bear University! ${whiteListString}`}</div>
+                    <WLRoundIndicator currentPhase={mintPhase} prices={puppetPrices} />
                     <br />
                     <MintCounter value={numToMint} setValue={setNumToMint} max={maxAllowedToMint} />
-                    <div className="font-semibold text-sm mb-1">{`Total: ${numToMint * puppetPrices[roundJoined]} AVAX`}</div>
+                    <div className="font-semibold text-sm mb-1">{`Total: ${totalCost} AVAX`}</div>
                     {isConnected
-                        ? (<MintButton />)
+                        ? (<Button
+                            className="snow-button max-w-[150px]"
+                            disabled={false && whiteListLevel >= mintPhase}
+                            onClick={handleMint}>Mint</Button>)
                         : (<ConnectButton />)}
                 </div>
             </div>
--- src/components/wl-round-indicator.tsx
+++ src/components/wl-round-indicator.tsx
@@ -1,23 +1,37 @@
 "use client"
 
-const rounds = [1, 2, 3, 4, 5];
+const rounds = [1, 2, 3, 4, 5, 6];
 
-export function WLRoundIndicator({ roundJoined, prices }: { roundJoined: number | null, prices: Record<number, number> }) {
+export function WLRoundIndicator({ currentPhase, prices }: { currentPhase: number, prices: Record<number, number> }) {
     return (
         <div className="flex flex-col items-center gap-4">
             <div className="flex justify-between w-full max-w-lg gap-3">
-                {rounds.map((round) => (
-                    <div key={round} className="flex flex-col items-center">
-                        <span className="text-lg font-semibold">{`WL${round}`}</span>
-                        <div
-                            className={`w-6 h-6 rounded-full border border-gray-500 ${roundJoined === round ? "bg-green-500" : "bg-red-500"
-                                }`}
-                        />
-                        <span className={`mt-2 text-sm ${roundJoined === round ? "text-green-500 font-bold" : "text-gray-600"}`}>
-                            {`${prices[round]} AVAX` || 0}
-                        </span>
-                    </div>
-                ))}
+                {rounds.map((round) => {
+                    if (round === 6) {
+                        return (<div key={round} className="flex flex-col items-center">
+                            <span className="text-lg font-semibold">{"Public"}</span>
+                            <div
+                                className={`w-6 h-6 rounded-full border border-gray-500 ${currentPhase === round ? "bg-green-500" : currentPhase > round ? "bg-red-500" : "bg-zinc-500"
+                                    }`}
+                            />
+                            <span className={`mt-2 text-sm ${currentPhase === round ? "text-green-500 font-bold" : "text-gray-600"}`}>
+                                {`${prices[round]} AVAX` || 0}
+                            </span>
+                        </div>);
+                    }
+                    return (
+                        <div key={round} className="flex flex-col items-center">
+                            <span className="text-lg font-semibold">{`WL${round}`}</span>
+                            <div
+                                className={`w-6 h-6 rounded-full border border-gray-500 ${currentPhase === round ? "bg-green-500" : currentPhase > round ? "bg-red-500" : "bg-zinc-500"
+                                    }`}
+                            />
+                            <span className={`mt-2 text-sm ${currentPhase === round ? "text-green-500 font-bold" : "text-gray-600"}`}>
+                                {`${prices[round]} AVAX` || 0}
+                            </span>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
