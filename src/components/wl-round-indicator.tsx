f194aaf1cf4de3d429b6ba42ba2d69e09eed6904
Mach1Btc <allxcaps@gmail.com>
2025-02-12 16:05:22 -0600

    modify client for new mint phasing, need to update the application view to be more of a landing page to connect discord before accessing the mint window

--- src/components/wl-round-indicator.tsx
+++ src/components/wl-round-indicator.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-const rounds = [1, 2, 3, 4, 5, 6];
+const rounds = [1, 2, 3, 4, 5];
 
 export function WLRoundIndicator({ currentPhase, prices }: { currentPhase: number, prices: Record<number, number> }) {
     return (
         <div className="flex flex-col items-center gap-4">
             <div className="flex justify-between w-full max-w-lg gap-3">
                 {rounds.map((round) => {
-                    if (round === 6) {
+                    if (round === 1) {
                         return (<div key={round} className="flex flex-col items-center">
-                            <span className="text-lg font-semibold">{"Public"}</span>
+                            <span className="text-lg font-semibold">{"WL"}</span>
                             <div
                                 className={`w-6 h-6 rounded-full border border-gray-500 ${currentPhase === round ? "bg-green-500" : currentPhase > round ? "bg-red-500" : "bg-zinc-500"
                                     }`}
@@ -21,7 +21,7 @@ export function WLRoundIndicator({ currentPhase, prices }: { currentPhase: numbe
                     }
                     return (
                         <div key={round} className="flex flex-col items-center">
-                            <span className="text-lg font-semibold">{`WL${round}`}</span>
+                            <span className="text-lg font-semibold">{`P${round - 1}`}</span>
                             <div
                                 className={`w-6 h-6 rounded-full border border-gray-500 ${currentPhase === round ? "bg-green-500" : currentPhase > round ? "bg-red-500" : "bg-zinc-500"
                                     }`}
