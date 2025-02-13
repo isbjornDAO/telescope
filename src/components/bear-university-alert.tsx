2f077d1b3e988acaae8e729ac5bf7ce9599fe712
Mach1Btc <allxcaps@gmail.com>
2025-02-13 15:54:03 -0600

    add open discord button to bear uni alert

--- src/components/bear-university-alert.tsx
+++ src/components/bear-university-alert.tsx
@@ -1,4 +1,5 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import { AlertCircle } from "lucide-react";
 
 
@@ -16,10 +17,21 @@ export function BearUniversityAlert() {
                             Enroll your puppet to Bear University.
                         </AlertTitle>
                         <AlertDescription className="text-zinc-500">
-                            Enroll your puppet and be rewarded for attending class!
+                            Enroll your puppet and be rewarded for attending class! Join our community for mint info
                         </AlertDescription>
                     </div>
                 </div>
+                <Button
+                    variant="outline"
+                    className="mt-4 md:mt-0 flex items-center gap-2"
+                    onClick={() => window.open('https://discord.gg/sBTs64xcbZ', '_blank')}
+                >
+                    <img
+                        src="/icons/discord.svg"
+                        className="h-4 w-4"
+                    />
+                    Open Discord
+                </Button>
             </Alert>
         </div>
     );
